717d811afa3481223d5446c18e5c786e3b119a4d
EknarongAphiphutthikul <akenarongake@hotmail.com>
2021-05-22 18:37:26 +0700

    get value from annotaion

--- src/main/java/com/example/aop/aspect/TimeAspectAop.java
+++ src/main/java/com/example/aop/aspect/TimeAspectAop.java
@@ -1,12 +1,16 @@
 package com.example.aop.aspect;
 
+import java.lang.reflect.Method;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.aop.annotation.TimeAnnotation;
 import com.example.aop.model.Request2Model;
 
 @Aspect
@@ -37,6 +41,10 @@ public class TimeAspectAop {
 	
 	@Around(value = "timeAnnotationPointCut() && execution(void com.example.aop.service.ServiceTest.*(..))")
 	public void timeVoidService(ProceedingJoinPoint pjp) throws Throwable {
+		MethodSignature signature = (MethodSignature) pjp.getSignature();
+	    Method method = signature.getMethod();
+	    TimeAnnotation timeAnno = method.getAnnotation(TimeAnnotation.class);
+		System.out.println("msg : " + timeAnno.message());
 		long t1 = System.currentTimeMillis();
 		pjp.proceed();
 		System.out.println("service void total time : " + (System.currentTimeMillis() - t1));
